ae99c2bfb31d63a5707db9d367a4dd8451f97cb9
Kiwi <himmu777paduri@gmail.com>
2022-02-03 01:02:30 +0530

    functions completed for scanning Wifis

--- app/src/main/java/com/example/iot_application/MainActivity.java
+++ app/src/main/java/com/example/iot_application/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.iot_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -9,6 +13,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,8 +38,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-
+                scanWifi();
             }
         });
+
+        listView = findViewById(R.id.wifiList);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if(!wifiManager.isWifiEnabled())
+        {
+            Toast.makeText(this,"Wifi is not enabled on your device.... Please enable it to scan", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        //scanWifi();
     }
+
+    private void scanWifi(){
+        arrayList.clear();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+        Toast.makeText(this, "Scanning Nearby WiFi Networks",Toast.LENGTH_SHORT).show();
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+
+            for(ScanResult scanResult : results){
+                arrayList.add(scanResult.SSID + "-" + scanResult.capabilities);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
 }
+
