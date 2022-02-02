4cf9814e9738f7fcb838e2b3115a55b4a16adb6d
Kiwi <himmu777paduri@gmail.com>
2022-02-02 20:37:42 +0530

    private variables added

--- app/src/main/java/com/example/iot_application/MainActivity.java
+++ app/src/main/java/com/example/iot_application/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.iot_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+    private ListView listView;
+    private Button buttonScan;
+    private int size = 0;
+    private List<ScanResult> results;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonScan = findViewById(R.id.scanButton);
+        buttonScan.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
     }
 }
